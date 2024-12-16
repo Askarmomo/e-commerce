2624691be56c7a6078c4fd0567fcf9c44f831d4b
Mohammed Askar <askarmomo111@gmail.com>
2024-12-16 16:33:59 +0530

    some ui changes

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -58,16 +58,16 @@ const Cart = () => {
             <div className=" space-y-4">
                 {
                     cart.map((product) => (
-                        < div key={product._id} className=" bg-white shadow-xl flex items-center justify-between sm:space-x-10 sm:w-[729px] h-fit rounded p-1 pr-4">
+                        < div key={product._id} className=" bg-white shadow-xl flex items-center justify-between sm:space-x-10 sm:w-[729px] h-fit rounded p-1 sm:pr-4">
                             <div className=" flex items-center pr-4">
                                 <div className=" fill-red-500 mr-2">
                                     <svg onClick={() => removeFromcart(product._id)} className=" fill-red-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path d="m8.4 16.308l3.6-3.6l3.6 3.6l.708-.708l-3.6-3.6l3.6-3.6l-.708-.708l-3.6 3.6l-3.6-3.6l-.708.708l3.6 3.6l-3.6 3.6zM12.003 21q-1.866 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709M12 20q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"></path></svg>
                                 </div>
                                 <img className=" pr-4 sm:w-40 w-[75px] sm:h-28 h-[75px] object-cover" src={product.image} alt={product.name} />
-                                <h1 className=" capitalize text-sm sm:text-base">{product.name}</h1>
+                                <h1 className=" capitalize text-xs sm:text-base">{product.name}</h1>
                             </div>
                             <div className=" flex items-center">
-                                <div className=" pr-4 flex items-center sm:space-x-10 space-y-2 text-center">
+                                <div className=" sm:pr-4 flex items-center sm:space-x-10 sm:space-y-2 text-center">
                                     <div>
                                         <span className=" text-teal-500 text-sm sm:text-base pr-2">Rs.{product.price}</span>
                                     </div>
