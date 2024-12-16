64e41409c7ae4f8af08256fddc3b55fd9d5fee58
Mohammed Askar <askarmomo111@gmail.com>
2024-12-16 09:51:57 +0530

    ui changes

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -6,8 +6,8 @@ const Categories = ({ img, title }) => {
 
 
     return (
-        <div className=" relative sm:max-w-fit max-w-[170px] max-h-[120px] sm:max-h-fit overflow-hidden cursor-pointer rounded-lg border-teal-500">
-            <img className=" sm:h-[300px] h-[200px] sm:w-[300px] w-[200px] object-cover hover:scale-105 transition-all duration-150 ease-in-out " src={img} alt={title} />
+        <div className=" relative sm:max-w-fit  max-h-[120px] sm:max-h-fit overflow-hidden cursor-pointer rounded-lg border-teal-500">
+            <img className=" sm:h-[300px] h-[200px] sm:w-[300px] object-cover hover:scale-105 transition-all duration-150 ease-in-out " src={img} alt={title} />
             <span className={` absolute font-poppins sm:pt-4 sm:pb-4 top-0 right-0  text-white bg-teal-700 bg-opacity-50 backdrop-blur-md sm:text-xl w-full text-center`} >{title}</span>
         </div>
     )
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -50,8 +50,7 @@ const HomePage = () => {
 
             <div>
                 <h1 className=" font-bold sm:text-4xl text-2xl text-center sm:pt-5 pb-1 bg-gradient-to-tr from-teal-800 to-teal-400 bg-clip-text text-transparent">Categories</h1>
-
-                <div data-aos="zoom-in" className=" sm:py-10 sm:pl-20 mx-12 my-5 grid grid-cols-2 sm:grid-cols-3 sm:space-y-5 sm:gap-10 gap-5 sm:items-center sm:justify-center">
+                <div data-aos="zoom-in" className=" mx-auto sm:py-10 sm:pl-20 px-5 my-5 grid grid-cols-2 sm:grid-cols-3 sm:space-y-5 sm:gap-10 gap-5 sm:items-center sm:justify-center">
                     {
 
                         CategoriesData.map((Category, index) => (
@@ -72,12 +71,11 @@ const HomePage = () => {
 
                     }
                 </div>
-
             </div>
 
             <div>
                 <h1 className=" sm:text-3xl text-2xl font-semibold text-teal-700 text-center" >Some Books</h1>
-                <div data-aos="zoom-in" className=" mt-10 grid sm:grid-cols-4 grid-cols-2 gap-5 sm:gap-20 sm:px-10 px-10 sm:py-10" >
+                <div data-aos="zoom-in" className=" mt-10 grid sm:grid-cols-4 grid-cols-2 gap-5 sm:gap-20 sm:px-10 px-4 sm:py-10" >
                     {
                         products.lenngth !== 0 && !products.message && products.map((product) => (
 
