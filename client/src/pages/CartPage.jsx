420d6f947c36651da2c3a612ae0febf87265270f
Mohammed Askar <askarmomo111@gmail.com>
2024-12-22 17:25:44 +0530

    ui update

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -2,7 +2,7 @@ import Cart from "../components/Cart"
 
 const CartPage = () => {
     return (
-        <div className="space-y-4 sm:mt-10 mt-[200px] mb-[200px] sm:ml-10">
+        <div className="space-y-4 sm:mt-10 mt-[200px] mb-[240px] sm:mb-auto sm:ml-10">
             <Cart />
         </div>
     )
